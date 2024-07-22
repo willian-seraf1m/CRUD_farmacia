3df8be7735e6337b69e6c26fcd51121a2301b10e
willian <willianserafim2017@gmail.com>
2024-07-22 10:28:41 -0300

    crud de categoria

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,37 @@
+package com.generation.farmacia.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "O Atributo Descrição é obrigatório")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+}
